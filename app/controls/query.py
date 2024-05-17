ba7a6f8fa6053cebb3afc08924f40506ff08e4a3
Dat <dat.lemindast@gmail.com>
2024-05-17 10:11:03 +0700

    udapte

--- app/controls/query.py
+++ app/controls/query.py
@@ -80,9 +80,7 @@ def optimizationQueryImageFailBarcode(table):
     connection = connectToSqlServer('DESKTOP-DGEHS9H', 'U-CheckDate-Barcode')
     cursor = connection.cursor()
     collection = ensure_collection_exists("U-CheckDate-Barcode-Stn", table)
-    
-    result = collection.delete_many({})
-    
+    collection.delete_many({})
     startdate = datetime.datetime(2023, 1, 1, 0, 0, 0)
     pipeline = f"""
     SELECT CONVERT(date, DateTime) AS date,
@@ -306,28 +304,33 @@ def optimizationQueryImageFailDateCode(table):
 
 def optimizationQueryImageFailLO1(table):
     """
-    result dataman
+    counter bottles server
     """
     connection = connectToSqlServer('DESKTOP-DGEHS9H', 'U-CheckDate-Barcode')
     cursor = connection.cursor()
     collection = ensure_collection_exists("U-CheckDate-Barcode-Stn", table)
     
     result = collection.delete_many({})
+    print(result)
+
     startdate = datetime.datetime(2023, 1, 1, 0, 0, 0)
-    query = f"""
+    # Define the pipeline string using f-strings for cleaner formatting
+    pipeline = f"""
     SELECT CONVERT(date, DateTime) AS date,
         DAY(DateTime) AS day,
         MONTH(DateTime) AS month,
         YEAR(DateTime) AS year,
-        Shift AS shift,
-		FGsCode AS sku,
-        ProductName AS productName,
-		Line As line,
+        CASE
+            WHEN DATEPART(hh, DateTime) < 6 THEN 1
+            WHEN DATEPART(hh, DateTime) < 14 THEN 2
+            ELSE 3
+        END AS shift,
+        FGsCode AS sku,
+        Camera AS camera,
         COUNT(*) AS count,
-        SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
-        SUM(CASE WHEN Status = 'Not Good' THEN 1 ELSE 0 END) AS countNotgood
+        COUNT(ImageFail) AS countFail
     FROM {table}
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode,
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode, Camera
         CASE
             WHEN DATEPART(hh, DateTime) < 6 THEN 1
             WHEN DATEPART(hh, DateTime) < 14 THEN 2
@@ -335,53 +338,61 @@ def optimizationQueryImageFailLO1(table):
         END
     ORDER BY date, shift;
     """
-    cursor.execute(query)
-    data = cursor.fetchall()
+    cursor.execute(pipeline)
+    group_data = cursor.fetchall()
+
     data_insert = []
-    for row in data:
+    for row in group_data:
         new_row = {
             "date": row[0],
-            "day": row[1],
+            "day": row [1],
             "month": row[2],
             "year": row[3],
             "shift": row[4],
             "sku":  row[5],
-            "name": row[6].
-            "line": row[7],
-            "count": row[8],
-            "countPass": row[9],
-            "countNotgood": row[10],
+            "camera": row[6],
+            "count": row[7],
+            "countFail": row[8],
         }
+        #for key, value in new_row.items():
+            #if key == "date":
+                #new_row[key] = dt.strftime(value, "%Y-%m-%d")
+            #else:
+                #new_row[key] = value if value != float('nan') else ""
         data_insert.append(new_row)
-    print(data_insert)
 
     collection.insert_many(data_insert)
     connection.close()
     
 def optimizationQueryImageFailLO2(table):
     """
-    result dataman
+    counter bottles server
     """
     connection = connectToSqlServer('DESKTOP-DGEHS9H', 'U-CheckDate-Barcode')
     cursor = connection.cursor()
     collection = ensure_collection_exists("U-CheckDate-Barcode-Stn", table)
-
+    
     result = collection.delete_many({})
+    print(result)
+
     startdate = datetime.datetime(2023, 1, 1, 0, 0, 0)
-    query = f"""
+    # Define the pipeline string using f-strings for cleaner formatting
+    pipeline = f"""
     SELECT CONVERT(date, DateTime) AS date,
         DAY(DateTime) AS day,
         MONTH(DateTime) AS month,
         YEAR(DateTime) AS year,
-        Shift AS shift,
-		FGsCode AS sku,
-        ProductName AS productName,
-		Line As line,
+        CASE
+            WHEN DATEPART(hh, DateTime) < 6 THEN 1
+            WHEN DATEPART(hh, DateTime) < 14 THEN 2
+            ELSE 3
+        END AS shift,
+        FGsCode AS sku,
+        Camera AS camera,
         COUNT(*) AS count,
-        SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
-        SUM(CASE WHEN Status = 'Not Good' THEN 1 ELSE 0 END) AS countNotgood
+        COUNT(ImageFail) AS countFail
     FROM {table}
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode,
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode, Camera
         CASE
             WHEN DATEPART(hh, DateTime) < 6 THEN 1
             WHEN DATEPART(hh, DateTime) < 14 THEN 2
@@ -389,31 +400,34 @@ def optimizationQueryImageFailLO2(table):
         END
     ORDER BY date, shift;
     """
-    cursor.execute(query)
-    data = cursor.fetchall()
+    cursor.execute(pipeline)
+    group_data = cursor.fetchall()
+
     data_insert = []
-    for row in data:
+    for row in group_data:
         new_row = {
             "date": row[0],
-            "day": row[1],
+            "day": row [1],
             "month": row[2],
             "year": row[3],
             "shift": row[4],
             "sku":  row[5],
-            "name": row[6].
-            "line": row[7],
-            "count": row[8],
-            "countPass": row[9],
-            "countNotgood": row[10],
+            "camera": row[6],
+            "count": row[7],
+            "countFail": row[8],
         }
+        #for key, value in new_row.items():
+            #if key == "date":
+                #new_row[key] = dt.strftime(value, "%Y-%m-%d")
+            #else:
+                #new_row[key] = value if value != float('nan') else ""
         data_insert.append(new_row)
-    print(data_insert)
 
     collection.insert_many(data_insert)
     connection.close()
 
 def querySqlServer():
-    optimizationQueryData("Table_Data") ## done
+    # optimizationQueryData("Table_Data") ## done
     optimizationQueryImageFailBarcode("Table_ImageFail_Barcode") ## done
     optimizationQueryImageFailCap1("Table_ImageFail_Cap1")
     optimizationQueryImageFailCap2("Table_ImageFail_Cap2")
